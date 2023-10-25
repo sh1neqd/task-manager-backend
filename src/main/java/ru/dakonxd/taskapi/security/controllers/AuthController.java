927dff4aa58dcc6e7f13914d88c774e5ad6fb4bf
sh1neqd <barton.g.bogdan@gmail.com>
2023-10-25 15:32:39 +0500

    adding swagger documentation

--- src/main/java/ru/dakonxd/taskapi/security/controllers/AuthController.java
+++ src/main/java/ru/dakonxd/taskapi/security/controllers/AuthController.java
@@ -1,5 +1,10 @@
 package ru.dakonxd.taskapi.security.controllers;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.servers.Server;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -9,16 +14,22 @@ import ru.dakonxd.taskapi.security.entities.dtos.JwtRequest;
 import ru.dakonxd.taskapi.security.entities.dtos.RegistrationUserDto;
 import ru.dakonxd.taskapi.security.service.AuthService;
 
+@OpenAPIDefinition(info = @Info(
+        title = "Task manager",
+        description = "<H4>microservice project</H4>"),
+        servers = {@Server(url = "http://localhost:8189", description = "Local server")})
+@Tag(name = "Create task!")
 @RestController
 @RequiredArgsConstructor
 public class AuthController {
     private final AuthService authService;
 
+    @Operation(summary = "Authorization endpoint")
     @PostMapping("/auth")
     public ResponseEntity<?> createAuthToken(@RequestBody JwtRequest authRequest) {
         return authService.createAuthToken(authRequest);
     }
-
+    @Operation(summary = "Registration endpoint")
     @PostMapping("/registration")
     public ResponseEntity<?> createNewUser(@RequestBody RegistrationUserDto registrationUserDto) {
         return authService.createNewUser(registrationUserDto);
--- src/main/java/ru/dakonxd/taskapi/taskmanager/controllers/TaskController.java
+++ src/main/java/ru/dakonxd/taskapi/taskmanager/controllers/TaskController.java
@@ -1,6 +1,7 @@
 package ru.dakonxd.taskapi.taskmanager.controllers;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.servers.Server;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -16,7 +17,6 @@ import ru.dakonxd.taskapi.taskmanager.service.TaskService;
         title = "Task manager",
         description = "<H4>microservice project</H4>"),
         servers = {@Server(url = "http://localhost:8189", description = "Local server")})
-
 @Tag(name = "Create task!")
 @RestController
 @AllArgsConstructor
@@ -24,24 +24,28 @@ public class TaskController {
 
     private final TaskService taskService;
 
+    @Operation(summary = "Task creating")
     @PostMapping("/createtask")
     public ResponseEntity<?> createTask(@RequestBody TaskDto taskDto) {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         return taskService.save(taskDto, authentication.getName());
     }
 
+    @Operation(summary = "Getting all tasks(only authorized")
     @GetMapping("/tasks")
     public ResponseEntity<?> getTasks() {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         return taskService.findTasksByUsername(authentication.getName());
     }
 
+    @Operation(summary = "Making task done(only authorized)")
     @PostMapping("/tasks/{taskname}/done")
     public ResponseEntity<?> makeTaskDone(@PathVariable("taskname") String taskname) {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         return taskService.makeTaskDone(taskname, authentication.getName());
     }
 
+    @Operation(summary = "Task creating(only authorized)")
     @PostMapping("/tasks/{taskname}/delete")
     public ResponseEntity<?> deleteTask(@PathVariable("taskname") String taskname) {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
